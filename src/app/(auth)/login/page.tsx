24d4615772921f661e019af9bc03302ee5533427
linhnvh15vn <linhnvh15vn@gmail.com>
2024-07-31 18:09:45 +0700

    feat: create login page

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export default function Page() {
+  return (
+    <main className="main">
+      <div className="login-form">
+        <h2 className="heading-secondary ma-bt-lg">Log into your account</h2>
+        <form className="form form--login">
+          <div className="form__group">
+            <label className="form__label" htmlFor="email">
+              Email address
+            </label>
+            <input
+              className="form__input"
+              id="email"
+              type="email"
+              placeholder="you@example.com"
+              required={true}
+            />
+          </div>
+          <div className="form__group ma-bt-md">
+            <label className="form__label" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="form__input"
+              id="password"
+              type="password"
+              placeholder="••••••••"
+              required={true}
+              minLength={8}
+            />
+          </div>
+          <div className="form__group">
+            <button className="btn btn--green">Login</button>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+}
