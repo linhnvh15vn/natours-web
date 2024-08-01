92c90f8e821ce149b91ac020bf0ec6c6e6ddbcff
linhnvh15vn <linhnvh15vn@gmail.com>
2024-08-01 12:10:48 +0700

    refactor: remove /public/style.css and move asset files to /public/images

---
+++ src/components/ui/button/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface Props {
+  // Add your component props here
+}
+
+export default function Button(props: Props) {
+  return <div>{/* Add your component content here */}</div>;
+}
